b65863697069e1e2acd86e0666f60b5224cecfe4
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-10 16:20:45 -0700

    fix output file prefix

--- src/calc_qihmt_map.sh
+++ src/calc_qihmt_map.sh
@@ -11,7 +11,7 @@ for i in $(cat /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/src/subjl
 	cd ${i}
 	3dcalc -datum float -a ${i}-Positive.nii[0] -b ${i}-Positive.nii[1] -c ${i}-Positive.nii[2] -expr '(a+b+c)/3' -prefix ${i}-Positive_Avg.nii #average of positive
 	3dcalc -datum float -a ${i}-Negative.nii[0] -b ${i}-Negative.nii[1] -c ${i}-Negative.nii[2] -expr '(a+b+c)/3' -prefix ${i}-Negative_Avg.nii #average of negative
-	3dcalc -datum float -a ${i}-Positive_Avg.nii -b ${i}-Negative_Avg.nii -expr '(a+b)/2' -prefix S_PosNeg.nii #average of negative and positive
+	3dcalc -datum float -a ${i}-Positive_Avg.nii -b ${i}-Negative_Avg.nii -expr '(a+b)/2' -prefix ${i}-S_PosNeg.nii #average of negative and positive
 	3dcalc -datum float -a ${i}-Dual.nii[0] -b ${i}-Dual.nii[1] -c ${i}-Dual.nii[2] -d ${i}-Dual.nii[3] -e ${i}-Dual.nii[4] -f ${i}-Dual.nii[5] -expr '(a+b+c+d+e+f)/6' -prefix ${i}-S_Dual.nii #average of dual
 	3dcalc -datum float -a ${i}-S_Dual.nii -expr '1/a' -prefix ${i}-1_S_Dual.nii #1/Sdual image
 	3dcalc -datum float -a ${i}-S_PosNeg.nii -expr '1/a' -prefix ${i}-1_S_PosNeg.nii #1/S+/- image
