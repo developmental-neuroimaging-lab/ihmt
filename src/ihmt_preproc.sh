e61e1ad3c73c4466cccc93d51be871d84e7d9068
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-10 14:36:24 -0700

    fixed for loop indexing

--- src/ihmt_preproc.sh
+++ src/ihmt_preproc.sh
@@ -6,19 +6,19 @@ cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
 
 #Run <Mcflirt on the IHMT .nii file
 
-for i in $(cat subjlist.txt); do
-	mcflirt -in ${i}/${i}-ihmt.nii.gz
-	done
+# for i in $(cat /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/src/subjlist.txt); do
+# 	mcflirt -in ${i}/${i}-ihmt.nii.gz
+# 	done
 
 #Calculate all the individual components of the maps (these don’t need to be done – this is just to know what volumes are part of each component)
 
-for id in $(cat subjlist.txt); do
+for i in $(cat /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/src/subjlist.txt); do
 	cd ${i}
-	3dcalc -a ${id}-ihmt_mcf.nii.gz[0] -expr 'a*1' -prefix MTR_Ref.nii
-	3dcalc -a ${id}-ihmt_mcf.nii.gz[1] -expr 'a*1' -prefix MT_Ref.nii
-	3dcalc -a ${id}-ihmt_mcf.nii.gz[2,6,10] -expr 'a*1' -prefix Positive.nii
-	3dcalc -a ${id}-ihmt_mcf.nii.gz[3,5,7,9,11,13] -expr 'a*1' -prefix Dual.nii
-	3dcalc -a ${id}-ihmt_mcf.nii.gz[4,8,12] -expr 'a*1' -prefix Negative.nii
+	3dcalc -a ${i}-ihmt_mcf.nii.gz[0] -expr 'a*1' -prefix ${i}-MTR_Ref.nii
+	3dcalc -a ${i}-ihmt_mcf.nii.gz[1] -expr 'a*1' -prefix ${i}-MT_Ref.nii
+	3dcalc -a ${i}-ihmt_mcf.nii.gz[2,6,10] -expr 'a*1' -prefix ${i}-Positive.nii
+	3dcalc -a ${i}-ihmt_mcf.nii.gz[3,5,7,9,11,13] -expr 'a*1' -prefix ${i}-Dual.nii
+	3dcalc -a ${i}-ihmt_mcf.nii.gz[4,8,12] -expr 'a*1' -prefix ${i}-Negative.nii
 	cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
 	done
 
