ad7332d75a1900e6f8821490e2e0f3bd2d745e36
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-14 10:52:24 -0700

    script for calculating MTR maps

---
+++ src/calc_mtr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#script by Jess Reynolds
+#updated by Meaghan Perdue Nov 2022 for new data
+
+cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
+
+
+#Calculate MTR maps for each subject for mask creation
+
+for i in $(cat /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/src/subjlist.txt); do
+	cd ${i}
+	3dcalc -datum float -a ${i}-S_PosNeg.nii -b ${i}-MTR_Ref.nii -expr '((b-a)/b)*10000' -prefix ${i}-MTR_Manual.nii	
+	cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
+	done
+	
