b3e22ad4fe20d1de7780056f436378d4527ac817
MeaghanPerdue <meaghan.perdue@ucalgary.ca>
2022-11-14 12:46:16 -0700

    fix loop indexing

--- src/swapdim_brainmasks.sh
+++ src/swapdim_brainmasks.sh
@@ -9,10 +9,10 @@ cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
 
 for i in $(cat /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/src/subjlist_usable.txt); do
 	cd ${i}
-	fslswapdim ${i}-MTR_Manual.nii RL PA IS ${i}_MTR_Manual_swap.nii	
-	bet2 ${id}_MTR_Manual_swap.nii ${id}_MTRBrain.nii -f 0.8
-	bet ${id}_MTRBrain.nii ${id}_MTRBrainR2.nii -R -m
-	fslswapdim ${id}-qihMT_Manual.nii RL PA IS ${id}_qihMT_swap.nii
-	fslmaths ${id}_qihMT_swap.nii -mas ${id}_MTRBrainR2_mask.nii ${id}_qihMTMasked.nii
+	#fslswapdim ${i}-MTR_Manual.nii RL PA IS ${i}_MTR_Manual_swap.nii	
+	bet2 ${i}_MTR_Manual_swap.nii ${i}_MTRBrain.nii -f 0.8
+	bet ${i}_MTRBrain.nii ${i}_MTRBrainR2.nii -R -m
+	fslswapdim ${i}-qihMT_Manual.nii RL PA IS ${i}_qihMT_swap.nii
+	fslmaths ${i}_qihMT_swap.nii -mas ${i}_MTRBrainR2_mask.nii ${i}_qihMTMasked.nii
 	cd /Volumes/catherine_team/mvperdue/preschool/ihMT_KatJess/new_data
 	done
